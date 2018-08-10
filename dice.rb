1f36abeb134477456a01277301efa9a7a7f6b6e2
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:15:10 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -1,14 +1,10 @@
 dice = [1, 2, 3, 4, 5, 6]
+erikas_rolls = []
 
-def dice_roll
-  dice = [1,2,3,4,5,6]
+
 counter = 0
 while counter < 5
-puts dice.sample
-counter += 1 
+erikas_rolls.push(dice.sample)
 end 
-end 
-dice_roll
 
-computer_rolls = [1,2,3,6,9,20]
-puts computer_rolls.sample
+
