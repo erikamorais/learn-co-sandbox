932f0feac10deeefaa088520defd38d669b8198a
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:32:05 +0000

    automatically backed up by learn

--- spirit.rb
+++ spirit.rb
@@ -1,2 +1,2 @@
 spirit_week = {}
-spirit_week ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] = pajama day, school pride, 
+spirit_week ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] = pajama day, school pride, we wear pink on wednesdays, 
