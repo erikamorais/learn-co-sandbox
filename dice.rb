304fca121fa76c96439e85cbe55ca0d1a96909d1
erikamorais <erikamorais2001@gmail.com>
2018-08-10 15:02:12 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -13,7 +13,7 @@ dice_rolls.each do |dice|
   #puts "#{dice}"
   total = total + dice
 end 
-puts total 
+#puts total 
 
 
 computer_array = [1, 2, 4, 6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
