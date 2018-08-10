9caf0264500279d53dd264b2ce1a2dc4083cac39
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:31:27 +0000

    automatically backed up by learn

--- spirit.rb
+++ spirit.rb
@@ -1,2 +1,2 @@
 spirit_week = {}
-spirit_week ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] = pj
+spirit_week ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] = pajama day, 
