ebfc98a83f4456cf22cd8d412e9aa595d47f259a
erikamorais <erikamorais2001@gmail.com>
2018-08-10 15:01:32 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -15,8 +15,6 @@ dice_rolls.each do |dice|
 end 
 puts total 
 
-dice_roll
-
 
 computer_array = [1, 2, 4, 6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
 puts total
@@ -29,3 +27,4 @@ if total > computer_roll
   puts "You lost :("
 end 
 end
+dice_roll
