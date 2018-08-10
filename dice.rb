f27c910742c416c93dd2417d2d47c35740d29db5
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:26:38 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -1,17 +1,19 @@
 def dice_roll
 dice = [1, 2, 3, 4, 5, 6]
-
+dice_rolls =[]
 counter = 0
 while counter < 5
-puts dice.sample
+dice_rolls.push (dice.sample)
 counter += 1 
 end 
 
 
 total = 0 
-dice.sample.each do |dice.sample|
-  puts "#{dice.sample}"
-  total = total = dice.sample
+dice_rolls.each do |dice|
+  puts "#{dice}"
+  total = total + dice
 end 
+puts total 
 end
+
 dice_roll
