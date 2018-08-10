2ad6bd5ea602cb305441cd4b6f5ea896dbb0835d
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:04:29 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -1,8 +1,11 @@
 dice = [1, 2, 3, 4, 5, 6]
 
+def dice_roll
+  dice = [1,2,3,4,5,6]
 counter = 0
-dice = 0 
-while dice < 5
+while counter < 5
 puts dice.sample
-dice += 1 
+counter += 1 
 end 
+end 
+dice_roll
