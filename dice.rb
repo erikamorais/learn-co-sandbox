645bf37fb9b37ed93960e9b21a9d0f8e33917d76
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:20:46 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -1,11 +1,13 @@
 dice = [1, 2, 3, 4, 5, 6]
-erikas_rolls = []
-
 
 counter = 0
 while counter < 5
-erikas_rolls.push(dice.sample)
+puts dice.sample
 counter += 1 
 end 
 
-puts erikas_rolls
+total = 0 
+dice.sample.each do |dice|
+  puts "#{dice.sample}"
+  total = total = dice.sample
+end 
