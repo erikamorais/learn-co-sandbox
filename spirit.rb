ec30ed9cec463a5bee74a473ab855b79a74b1253
erikamorais <erikamorais2001@gmail.com>
2018-08-10 14:38:55 +0000

    automatically backed up by learn

--- spirit.rb
+++ spirit.rb
@@ -1,6 +1,6 @@
 
-spirit_week ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-options ["pajama day", "school pride", "we wear pink on wednesdays", "disney/movie characters", "fancy friday"]
+spirit_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+options = ["pajama day", "school pride", "we wear pink on wednesdays", "disney/movie characters", "fancy friday"]
 spirit_hash = {}
 counter = 0 
 spirit_week.each do |days|
